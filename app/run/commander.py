274815efc7dc0c97a41c283c03b739c55b2d1975
nishu <nishu@foxmail.com>
2018-11-07 11:51:36 +0800

    update

--- app/run/commander.py
+++ app/run/commander.py
@@ -20,16 +20,16 @@ if __name__ == '__main__':
 
     p1 = multiprocessing.Process(target=robotFactory, args=(lock, config_food))
     p2 = multiprocessing.Process(target=robotFactory, args=(lock, config_history))
-    # p3 = multiprocessing.Process(target=robotFactory, args=(lock, config_joke))
+    p3 = multiprocessing.Process(target=robotFactory, args=(lock, config_joke))
     p4 = multiprocessing.Process(target=robotFactory, args=(lock, config_pet))
 
     p1.start()
     p2.start()
-    # p3.start()
+    p3.start()
     p4.start()
 
     p1.join()
     p2.join()
-    # p3.join()
+    p3.join()
     p4.join()
     
