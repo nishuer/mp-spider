ae6d400ffe48da3d4938631ad879f6aeb58832c0
nishu <nishu@foxmail.com>
2018-11-08 13:02:04 +0800

    update

--- app/rule/kuaibao_rule.py
+++ app/rule/kuaibao_rule.py
@@ -33,8 +33,12 @@ def hideOtherElement(self):
     e4s = driver.find_elements_by_xpath('//*[@id="content"]/*[position() < 3]')
 
     e1 = driver.find_element_by_xpath('//*[@id="top_downloader"]')
-    e2 = driver.find_element_by_xpath('//*[@class="downloader-bar"]')
 
+    try:
+        e2 = driver.find_element_by_xpath('//*[@class="downloader-bar"]')
+    except Exception as e:
+        print(e)
+ 
     for _ in e1s:
         driver.execute_script("arguments[0].setAttribute('style', 'display: none')", _)
     for _ in e2s:
@@ -45,4 +49,6 @@ def hideOtherElement(self):
         driver.execute_script("arguments[0].setAttribute('style', 'display: none')", _)
 
     driver.execute_script("arguments[0].setAttribute('style', 'display: none')", e1)
-    driver.execute_script("arguments[0].setAttribute('style', 'display: none')", e2)
+
+    if (e2):
+        driver.execute_script("arguments[0].setAttribute('style', 'display: none')", e2)
