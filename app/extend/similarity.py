544c269d7272f8e020a7255612642af6b3db23c8
nishuer <nishu@foxmail.com>
2018-11-28 10:35:18 +0800

    update

--- app/extend/similarity.py
+++ app/extend/similarity.py
@@ -7,6 +7,7 @@ def titleCompare(title, target_title):
     res = difflib.SequenceMatcher(None, title, target_title).quick_ratio()
 
     try:
+        pass
         # titleSimilarityLog(title, target_title, res)
     finally:
         return (res < TITLE_SIMILARITY_THRESHOLD) if TITLE_SIMILARITY_SWITCH else True
