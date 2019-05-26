ae013077c12f54bfcef02d5a7d40452ea7b16e47
nishuer <nishu@foxmail.com>
2019-05-26 23:36:48 +0800

    update

--- app/api/request.py
+++ app/api/request.py
@@ -1,8 +1,8 @@
 import requests
 
 
-def get(url, params={}, cookies={}, headers={}, **kwargs):
-    res = requests.get(url, params=params, headers=headers, cookies=cookies, **kwargs)
+def get(url, params={}, cookies={}, headers={}, timeout=10, **kwargs):
+    res = requests.get(url, params=params, headers=headers, cookies=cookies, timeout=timeout, **kwargs)
     print(res.status_code)
     
     try:
@@ -12,8 +12,8 @@ def get(url, params={}, cookies={}, headers={}, **kwargs):
         return None
 
 
-def post(url, params={}, cookies={}, headers={}, **kwargs):
-    res = requests.post(url, data=params, headers=headers, cookies=cookies, **kwargs)
+def post(url, params={}, cookies={}, headers={}, timeout=10, **kwargs):
+    res = requests.post(url, data=params, headers=headers, cookies=cookies, timeout=timeout, **kwargs)
     print(res.status_code)
     
     try:
